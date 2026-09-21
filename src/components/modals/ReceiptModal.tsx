29fc768116df915487edc578c04a804821d7eb19
nestoradolfo17-sudo <nestoradolfo17@gmail.com>
2026-09-21 12:07:01 -0600

    Update ReceiptModal.tsx

--- src/components/modals/ReceiptModal.tsx
+++ src/components/modals/ReceiptModal.tsx
@@ -370,6 +370,10 @@ export const ReceiptModal: React.FC<ReceiptModalProps> = ({
                       <strong className="text-slate-800">{route.mercado || 'Mercado Abierto'}</strong>
                     </div>
                     <div>
+                      <span className="text-[10px] text-slate-400 uppercase font-semibold block">Segmento:</span>
+                      <strong className="text-slate-800">{route.segmento || '-'}</strong>
+                    </div>
+                    <div>
                       <span className="text-[10px] text-slate-400 uppercase font-semibold block">Modalidad de Viaje:</span>
                       <strong className="text-slate-800">
                         {(() => {
