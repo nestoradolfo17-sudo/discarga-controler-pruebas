835ea0342d27ed11516708a475847a0ce5946a89
nestoradolfo17-sudo <nestoradolfo17@gmail.com>
2026-09-23 11:26:33 -0600

    Create segmentos.ts

---
+++ src/data/segmentos.ts
@@ -0,0 +1,3 @@
+// Segmentos disponibles al cargar rutas desde Excel (Carga Masiva de Rutas).
+// El segmento elegido se asigna a TODAS las rutas del archivo cargado.
+export const SEGMENTO_OPTIONS: string[] = ['C-STORE', 'POSTMIX', 'MERCADO ABIERTO', 'MAYOREO'];
